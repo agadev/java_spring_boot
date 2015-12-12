d2d8fdc2ce8420e4a38ab622841a89acfe49d2ce
atul <amoliksoft@gmail.com>
2015-12-12 20:46:34 +0530

    Adding new file

---
+++ AmolikDataProjects/src/main/java/com/amolik/data/OdishaRationCardBean.java
@@ -0,0 +1,43 @@
+package com.amolik.data;
+
+public class OdishaRationCardBean {
+
+	private String siNo;
+	private String gpOrWardNo;
+	private String villageOrLocality;
+	private String hamletOrPlotNo;
+	private String licenseHolderName;
+	public String getSiNo() {
+		return siNo;
+	}
+	public void setSiNo(String siNo) {
+		this.siNo = siNo;
+	}
+	public String getGpOrWardNo() {
+		return gpOrWardNo;
+	}
+	public void setGpOrWardNo(String gpOrWardNo) {
+		this.gpOrWardNo = gpOrWardNo;
+	}
+	public String getVillageOrLocality() {
+		return villageOrLocality;
+	}
+	public void setVillageOrLocality(String villageOrLocality) {
+		this.villageOrLocality = villageOrLocality;
+	}
+	public String getHamletOrPlotNo() {
+		return hamletOrPlotNo;
+	}
+	public void setHamletOrPlotNo(String hamletOrPlotNo) {
+		this.hamletOrPlotNo = hamletOrPlotNo;
+	}
+	public String getLicenseHolderName() {
+		return licenseHolderName;
+	}
+	public void setLicenseHolderName(String licenseHolderName) {
+		this.licenseHolderName = licenseHolderName;
+	}
+	
+	
+	
+}
