391122e93fbd9869ce29632fa676a668586dbf0a
atul <amoliksoft@gmail.com>
2016-01-17 20:36:07 +0530

    Added method to read excel column

--- AmolikDataProjects/src/main/java/com/amolik/util/ExcelUtil.java
+++ AmolikDataProjects/src/main/java/com/amolik/util/ExcelUtil.java
@@ -1,16 +1,27 @@
 package com.amolik.util;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import net.sf.jett.transform.ExcelTransformer;
 
@@ -28,114 +39,126 @@ import net.sf.jett.transform.ExcelTransformer;
  */
 
 public class ExcelUtil {
-    public static SimpleDateFormat dateAndTimeFormat = 
-	new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a"); 
-    
-    public static SimpleDateFormat usDateFormat = 
-	new SimpleDateFormat("MM/dd/yyyy");
-    
-    
-//  ---------------------------------------------------------------------------
-//  ---------------------------------------------------------------------------
-    /**
-     *  Generates Excel with given formatFile.
-     *  @param beanList - Contains List of Data Bean
-     *  @param excelTemplateFileName - Template excel file which contains output format.
-     *  @param excelTemplateFileName - Template excel file which contains output format when no data.
-     *  @param excelDestFileName - Full Name including path of the output excel file.
-     *  @param beanName - Name of the bean used to access fields in spreadsheet.
-     */
-    public static void generateExcelFromBean(List beanList,
-	    String excelTemplateFileName, 
-	    String noDataExcelTemplateFileName,
-	    String excelDestFileName,
-	    String beanName)
-    throws IOException, InvalidFormatException {
+	public static SimpleDateFormat dateAndTimeFormat = 
+			new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a"); 
+
+	public static SimpleDateFormat usDateFormat = 
+			new SimpleDateFormat("MM/dd/yyyy");
 
 
-	Map<String, Object> beans = new HashMap<String, Object>();
-	beans.put(beanName, beanList);
-	java.util.Date currentDate = new java.util.Date(System.currentTimeMillis());
-	String runDateTime = dateAndTimeFormat.format(currentDate);
-	beans.put("runDateTime", runDateTime);
-	
-	//String businessDateString = usDateFormat.format(businessDate);
-	//beans.put("businessDate", businessDateString);
-	//beans.put("clientName", clientName);
-	
-	ExcelTransformer transformer = new ExcelTransformer();
+	//  ---------------------------------------------------------------------------
+	//  ---------------------------------------------------------------------------
+	/**
+	 *  Generates Excel with given formatFile.
+	 *  @param beanList - Contains List of Data Bean
+	 *  @param excelTemplateFileName - Template excel file which contains output format.
+	 *  @param excelTemplateFileName - Template excel file which contains output format when no data.
+	 *  @param excelDestFileName - Full Name including path of the output excel file.
+	 *  @param beanName - Name of the bean used to access fields in spreadsheet.
+	 */
+	public static void generateExcelFromBean(List beanList,
+			String excelTemplateFileName, 
+			String noDataExcelTemplateFileName,
+			String excelDestFileName,
+			String beanName)
+					throws IOException, InvalidFormatException {
 
-	// Create excel file with some data
-	if (beanList.size()>0) {
-	    transformer.transform(excelTemplateFileName, excelDestFileName,beans);
+
+		Map<String, Object> beans = new HashMap<String, Object>();
+		beans.put(beanName, beanList);
+		java.util.Date currentDate = new java.util.Date(System.currentTimeMillis());
+		String runDateTime = dateAndTimeFormat.format(currentDate);
+		beans.put("runDateTime", runDateTime);
+
+		//String businessDateString = usDateFormat.format(businessDate);
+		//beans.put("businessDate", businessDateString);
+		//beans.put("clientName", clientName);
+
+		ExcelTransformer transformer = new ExcelTransformer();
+
+		// Create excel file with some data
+		if (beanList.size()>0) {
+			transformer.transform(excelTemplateFileName, excelDestFileName,beans);
+		}
+
+		// Check if needs to create excel file using no data template
+		else {
+
+			File physicalNoDataExcelTemplateFileName = 
+					new File(noDataExcelTemplateFileName);
+
+
+			// Check if noDataExcelTemplateFileExists, if yes then create
+			// excel file using that nodata excel template
+			if(physicalNoDataExcelTemplateFileName!=null &&
+					physicalNoDataExcelTemplateFileName.exists()) {
+
+				transformer.transform(noDataExcelTemplateFileName, 
+						excelDestFileName,beans);
+			}
+
+			// Create excel file using regular template
+			else {
+
+				transformer.transform(excelTemplateFileName,
+						excelDestFileName,beans);
+			}
+
+		}
 	}
 
-	// Check if needs to create excel file using no data template
-	else {
+	//  ---------------------------------------------------------------------------
+	//  ---------------------------------------------------------------------------
+	
 
-	    File physicalNoDataExcelTemplateFileName = 
-		new File(noDataExcelTemplateFileName);
+	public static Set<String> getExcelContentByColumnIndex(String excelFilePath,
+			int columnIndex){
+		
+		Set<String> columndata = new HashSet<String>(6000);
+		try {
+			File f = new File(excelFilePath);
+			FileInputStream ios = new FileInputStream(f);
+			
+			// for xlsx use 2 lines below
+			//XSSFWorkbook workbook = new XSSFWorkbook(ios);
+			//XSSFSheet sheet = workbook.getSheetAt(0);
+			
+			// for xls use this
+			org.apache.poi.ss.usermodel.Workbook workbook = WorkbookFactory.create(ios);
+			org.apache.poi.ss.usermodel.Sheet sheet = workbook.getSheetAt(0);
+			
+			Iterator<Row> rowIterator = sheet.iterator();
+			
 
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
 
-	    // Check if noDataExcelTemplateFileExists, if yes then create
-	    // excel file using that nodata excel template
-	    if(physicalNoDataExcelTemplateFileName!=null &&
-		    physicalNoDataExcelTemplateFileName.exists()) {
-
-		transformer.transform(noDataExcelTemplateFileName, 
-			 excelDestFileName,beans);
-	    }
-
-	    // Create excel file using regular template
-	    else {
-
-		transformer.transform(excelTemplateFileName,
-			 excelDestFileName,beans);
-	    }
-
+					if(row.getRowNum() > 0){ //To filter column headings
+						if(cell.getColumnIndex() == columnIndex){// To match column index
+							switch (cell.getCellType()) {
+							case Cell.CELL_TYPE_STRING:
+								
+								Path p = Paths.get(cell.getStringCellValue());
+								//System.out.println(p.getFileName().toString());
+								columndata.add(p.getFileName().toString());
+								break;
+							}
+						}
+					}
+				}
+			}
+			ios.close();
+			//System.out.println(columndata);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return columndata;
 	}
-    }
+	//  ---------------------------------------------------------------------------
+	//  ---------------------------------------------------------------------------   
 
-//  ---------------------------------------------------------------------------
-//  ---------------------------------------------------------------------------
-    /**
-     *  Generates Excel with given formatFile with contains jxls based sql query.
-     *  The fields in query are accessed using rm prefix. eg rm.price,rm.qty etc.
-     *  @param excelTemplateFileName - Template excel file which contains output format.
-     *  @param excelDestFileName - Full Name including path of the output excel file.
-     *  @param businessDate - Formatted business date string. 
-     *  @parma connectionType - Either oracle or db2.
-     */
-//    private void generateExcel(
-//	    String excelTemplateFileName,
-//	    String excelDestFileName,
-//	    String businessDate,
-//	    String connectionType) 
-//    throws IOException {
-//
-//	Connection dbConnection = null;
-//	try {
-//	    
-//	    dbConnection = DbUtil.getConnection(connectionType);
-//	    Map beans = new HashMap();
-//	    ReportManager reportManager = new ReportManagerImpl( dbConnection, beans );
-//	    beans.put("rm", reportManager);
-//	    java.util.Date currentDate = new java.util.Date(System.currentTimeMillis());
-//	    String runDateTime = dateAndTimeFormat.format(currentDate);
-//	    beans.put("runDateTime", runDateTime);
-//	    beans.put("businessDate", businessDate);
-//	    XLSTransformer transformer = new XLSTransformer();
-//	    transformer.transformXLS(excelTemplateFileName, beans, excelDestFileName);
-//	} catch (SQLException e) {
-//	   
-//	    e.printStackTrace();
-//	}
-//
-//	finally {
-//	    
-//	   
-//	}
-//    }
-//  ---------------------------------------------------------------------------
-//  ---------------------------------------------------------------------------   
+	
 }
