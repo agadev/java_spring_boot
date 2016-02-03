9d07912b1563842ae67ea935f1b948e5a83f2c7c
atul <amoliksoft@gmail.com>
2016-02-03 16:16:44 +0530

    Added correction field file generation

--- AmolikDataProjects/src/main/java/com/amolik/util/ExcelUtil.java
+++ AmolikDataProjects/src/main/java/com/amolik/util/ExcelUtil.java
@@ -15,6 +15,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
@@ -22,6 +23,9 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.amolik.data.FiscalCorrectionField;
+import com.amolik.data.FiscalRecord;
 
 import net.sf.jett.transform.ExcelTransformer;
 
@@ -158,7 +162,253 @@ public class ExcelUtil {
 		return columndata;
 	}
 	//  ---------------------------------------------------------------------------
-	//  ---------------------------------------------------------------------------   
+	//  ---------------------------------------------------------------------------  
+	
+	public static Map<String,FiscalRecord> getFiscalRecordsFromExcel(String excelFilePath){
+		
+		Map<String,FiscalRecord> fiscalRecordMap = new TreeMap<String,FiscalRecord>();
+		try {
+			File f = new File(excelFilePath);
+			FileInputStream ios = new FileInputStream(f);
+						
+			// for xls use this
+			org.apache.poi.ss.usermodel.Workbook workbook = WorkbookFactory.create(ios);
+			org.apache.poi.ss.usermodel.Sheet sheet = workbook.getSheetAt(0);
+			
+			Iterator<Row> rowIterator = sheet.iterator();
+			
 
+			while (rowIterator.hasNext()) {
+				
+				FiscalRecord record = new FiscalRecord();
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+
+					if(row.getRowNum() > 0){ //To filter column headings
+					
+						switch(cell.getColumnIndex()) {
+						
+						case 0:
+							   break;
+						case 1:record.setImageFileName(cell.getStringCellValue());
+							   break;
+							   
+						case 2:record.setSrNo(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 3:record.setEmpIdNo(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 4:record.setOccuranceNo(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 5:record.setLoanFileNo(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 6:record.setLoanAmount(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 7:record.setRateOfInterest(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 8:record.setTenure(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 9:record.setTotalLoan(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 10:record.setEmi(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 11:record.setOtherLoans(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 12:record.setInitials(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 13:record.setEmpName(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 14:record.setAddress(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 15:record.setCity(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 16:record.setState(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 17:record.setZip(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 18:record.setCountry(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 19:record.setContactMode(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 20:record.setMaritalStatus(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 21:record.setRefName(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 22:record.setYearsOfEmployment(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 23:record.setDesignation(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 24:record.setDepartment(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 25:record.setPerformance(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 26:record.setBasicSalary(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 27:record.setCenterName(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 28:record.setIssuerBank(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+							   
+						case 29:record.setEisCode(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+							   break;
+						
+						case 30:record.setCarrierName(
+								FiscalEncrypterDecrypter.DecryptFiscalText(
+								cell.getStringCellValue()));
+						        
+						        fiscalRecordMap.put(record.getImageFileName(),record);
+						        break;
+							   
+						}
+					}
+				}
+			}
+			ios.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fiscalRecordMap;
+	}
+
+public static List<FiscalCorrectionField> getCorrectFiscalFieldsFromExcel(String excelFilePath){
+		
+		List<FiscalCorrectionField> fiscalCorrectionFieldList = 
+				                   new ArrayList<FiscalCorrectionField>(6000);
+		try {
+			File f = new File(excelFilePath);
+			FileInputStream ios = new FileInputStream(f);
+						
+			// for xls use this
+			org.apache.poi.ss.usermodel.Workbook workbook = WorkbookFactory.create(ios);
+			org.apache.poi.ss.usermodel.Sheet sheet = workbook.getSheetAt(0);
+			
+			Iterator<Row> rowIterator = sheet.iterator();
+			
+
+			while (rowIterator.hasNext()) {
+				
+				FiscalCorrectionField record = new FiscalCorrectionField();
+				Row row = rowIterator.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+
+					if(row.getRowNum() > 3){ //To filter column headings
+					
+						switch(cell.getColumnIndex()) {
+						
+						case 0:
+							   break;
+						case 1:record.setImageName(
+								StringUtility.trim(cell.getStringCellValue()));
+							   break;
+							   
+						case 2:record.setFieldName(
+								StringUtility.trim(cell.getStringCellValue()));
+							   break;
+							   
+						case 3:record.setCorrectFieldValue(
+							   StringUtility.trim(cell.getStringCellValue()));
+						       fiscalCorrectionFieldList.add(record);
+							   break;
+							   
+						case 4:
+							  break;
+							   
+						}
+					}
+				}
+			}
+			ios.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fiscalCorrectionFieldList;
+	}
 	
 }
