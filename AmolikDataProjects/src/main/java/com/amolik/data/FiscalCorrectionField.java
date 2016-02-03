ca644255a568611b6e19ddafd1b6035173ab6d10
atul <amoliksoft@gmail.com>
2016-02-03 16:11:54 +0530

    Correction Data holder

---
+++ AmolikDataProjects/src/main/java/com/amolik/data/FiscalCorrectionField.java
@@ -0,0 +1,29 @@
+package com.amolik.data;
+
+public class FiscalCorrectionField {
+
+	private String imageName;
+	private String fieldName;
+	private String correctFieldValue;
+	
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public String getFieldName() {
+		return fieldName;
+	}
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+	public String getCorrectFieldValue() {
+		return correctFieldValue;
+	}
+	public void setCorrectFieldValue(String correctFieldValue) {
+		this.correctFieldValue = correctFieldValue;
+	}
+	
+	
+}
